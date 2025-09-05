f03d1f3db1d613a558d5d0474a133436ef944d6e
sujitmoharana <linkumaharana2@gmail.com>
2025-09-05 20:43:30 +0530

    github login on frontend part

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,30 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { GithubIcon } from 'lucide-react'
-import React from 'react'
+import { authClient } from '@/lib/auth-client'
+import { GithubIcon, Loader } from 'lucide-react'
+import React, { useTransition } from 'react'
+import { toast } from 'sonner'
 
 const loginPage = () => {
+  const [githubpending,startGithubTransition] = useTransition()
+const signInWithGithub = ()=>{
+  startGithubTransition(async()=>{        // https://chatgpt.com/s/t_68baa79c0e308191aa7ece475ee67503 
+    await authClient.signIn.social({
+      provider:"github",
+      callbackURL:"/",
+      fetchOptions:{
+        onSuccess:()=>{
+          toast.success('signed in with github,you will be redirected... ')
+        },
+        onError:(error)=>{
+          toast.error("internal server error")
+        }
+      }})
+   })
+}
   return (
     <Card>
         <CardHeader>
@@ -13,8 +32,18 @@ const loginPage = () => {
           <CardDescription>Login with your Github Email Account</CardDescription>
         </CardHeader>
         <CardContent className='flex flex-col gap-4'>
-            <Button className='w-full' variant ='outline'>
-            <GithubIcon/> Sign in with Github
+            <Button disabled={githubpending} onClick={signInWithGithub} className='w-full' variant ='outline'>
+           {githubpending?(
+            <>
+            <Loader className="size-4 animate-spin"/>
+            <span>Loading...</span>
+            </>
+           ):(
+            <>
+            <GithubIcon className='size-4'/>
+            Sign in with Github
+            </>
+           )}
             </Button>
             <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
               <span className='relative z-10 bg-card px-2 text-muted-foreground'>Or continue with</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import { ThemeToggle } from '@/components/ui/ThemeToggle'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
 import React from 'react'
 
-const Page = () => {
+const  Page = async() => {
+
+  const session = await auth.api.getSession({
+    headers:await headers()
+  })
+  console.log(session);
+  
   return (
     <div>
     <div>hello world</div>
     <ThemeToggle/>
+    {session?(
+      <p>{session.user.name}</p>
+    ):(
+      <button>Logout</button>
+    )}
     </div>
   )
 }
