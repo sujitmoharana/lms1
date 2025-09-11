3d308f9f67cfb24759a73034647950eb0d6b1961
sujitmoharana <linkumaharana2@gmail.com>
2025-09-11 13:08:01 +0530

    some chage in component ......

--- components/sidebar/site-header.tsx
+++ components/sidebar/site-header.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { ThemeToggle } from "./ui/ThemeToggle"
 
 export function SiteHeader() {
   return (
@@ -12,9 +11,18 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">SujitLMS.</h1>
+        <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
-         <ThemeToggle/>
+          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+            <a
+              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="dark:text-foreground"
+            >
+              GitHub
+            </a>
+          </Button>
         </div>
       </div>
     </header>
