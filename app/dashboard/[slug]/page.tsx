0b0ddbb67571ff02dc6e52ad519ac776863b0b4c
sujitmoharana <linkumaharana2@gmail.com>
2025-12-02 19:21:59 +0530

    if i go to slug route then i directly redirect into /dashboard/slug/lessonId

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,9 +1,26 @@
+import { getCourseSidebarDate } from '@/app/data/course/get-course-sidebar-data';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const CourseSlugRoute = () => {
+
+interface iAppProps{
+  params:Promise<{slug:string}>
+}
+const CourseSlugRoute = async({params}:iAppProps) => {
+  const {slug} =await params;
+
+  const course = await getCourseSidebarDate(slug)
+
+  const firstChapter = course.courses.chapter[0]
+  const firstlesson = firstChapter.lessons[0]
+
+  if (firstlesson) {
+    redirect(`/dashboard/${slug}/${firstlesson.id}`)
+  }
   return (
-    <div>
-         <h1>helo world</h1>
+    <div className='flex items-center justify-center h-full text-center'>
+         <h2 className='text-2xl font-bold mb-2'>No Lesson avalaible</h2>
+         <p className='text-muted-foreground'>this course does not have any lessons yet!</p>
     </div>
   )
 }
